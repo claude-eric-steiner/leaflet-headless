ac7faa687c9ace12e2359a0bbcd823b24351bf66
claude-eric-steiner <claude.e.steiner@gmail.com>
2023-05-05 13:32:36 +0200

    Update image.js

--- src/image.js
+++ src/image.js
@@ -51,7 +51,7 @@ Image.prototype.__defineSetter__('src', function (src) {
     switch (src.substr(0, 7)) {
     case 'https:/':
         console.log(src);
-        request.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0');
+        request.setHeader('User-Agent', ['Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0']);
         request.get(src, function (err, res, buffer) {
             if (err) {
                 console.error('Could not get url', err);
