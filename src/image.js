2e8afd3e06a330ae034b578829d54cbb3acc5114
claude-eric-steiner <claude.e.steiner@gmail.com>
2023-05-05 13:48:38 +0200

    Update image.js

--- src/image.js
+++ src/image.js
@@ -54,6 +54,7 @@ Image.prototype.__defineSetter__('src', function (src) {
         var options = {
             url: src,
             headers: {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'
             }
         }
